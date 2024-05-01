4d655c9fc5fc39dce521e6312b6ac8a421fe33b7
kacper <kacper070302@gmail.com>
2024-05-02 00:00:59 +0200

    login register

--- base/views.py
+++ base/views.py
@@ -5,6 +5,28 @@ from django.shortcuts import redirect, render, redirect
 from .forms import TaskForm
 from .models import Tasks
 from django.http import HttpResponse
+from django.contrib.auth.models import User
+from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
+def loginPage(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+    try:
+        user = User.objects.get(username=username)
+    except:
+        messages.error(request, "użytkownik nie istnieje")
+    user = authenticate(request, username=username, password=password)
+
+    if user is not None:
+        login(request, user)
+        return redirect('home')
+    else:
+        messages.error(request,"username or password incorrect")
+
+    context = {}
+    return render(request,'login_register.html',context)
+
 def tasksFormView(request):
     task = Tasks.objects.get() #tu moze cos dodac trzeba
     context = {'task':task}
