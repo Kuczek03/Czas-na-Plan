99ad04360bc1ca93c3a503ff771a03a32244f099
Pawel Lopocki <pawellopocki@gmail.com>
2024-05-05 23:24:59 +0200

    da sie dodac boarda

--- base/models.py
+++ base/models.py
@@ -13,7 +13,7 @@ class Tasks(models.Model):
     task_status=models.CharField(max_length=20)
     board_id=models.ForeignKey('Dashboards', on_delete=models.CASCADE, null=True)
 
-    def __str__(self):
+    def __str__(self):                                                                                                                                                     
         return self.task_name
 
 # class Users(models.Model):
@@ -49,7 +49,7 @@ class Label_tasks(models.Model):
 class Dashboards(models.Model):
     dashboard_id=models.AutoField(primary_key=True)
     dashboard_name=models.CharField(max_length=200)
-    dashboard_admin_id=models.ForeignKey(User,on_delete=models.CASCADE)
+    dashboard_admin_id=models.ForeignKey(User,on_delete=models.DO_NOTHING,db_column='admin_id')
 
     def __str__(self):
         return self.dashboard_name
